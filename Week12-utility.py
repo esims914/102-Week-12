3844854ff43cf5dab8ce9080ebdb23ac03d60645
Trey Sims <esims@mymail.mines.edu>
2019-11-18 12:50:54 -0700

    added FindWordCount function

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,5 +1,4 @@
 
-
 def PrintOutput(string):
     print("OUTPUT " + string)
     
@@ -8,7 +7,7 @@ def LoadFile(fileloc):
     alist = []
     for line in f:
         alist.append(line)
-    print(alist)
+    return alist
     f.close()
 
 def UpdateString(string, char, pos):
@@ -18,4 +17,23 @@ def UpdateString(string, char, pos):
     newstr += char
     for i in range(len(newstr)-1+len(char),len(string)):
         newstr += string[i]
-    PrintOutput(newstr)
+    return newstr
+
+def FindWordCount(alist,astr):
+    numb = 0
+    for i in range(len(astr)):
+        alist += ' '
+    for item in alist:
+        letterloc = -1
+        for letter in item:
+            letterloc += 1
+            if letter == astr[0]:
+                tempcounter = 0
+                for j in range(len(astr)):
+                    if item[letterloc+j] != astr[j]:
+                        break
+                    else:
+                        tempcounter += 1
+                if tempcounter == len(astr):
+                    numb += 1
+    return numb
