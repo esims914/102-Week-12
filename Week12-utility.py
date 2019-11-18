3e40187b7abb9e4f0577a7edc29d865a4929bf72
Trey Sims <esims@mymail.mines.edu>
2019-11-18 12:22:45 -0700

    added file thing

--- Week12-utility.py
+++ Week12-utility.py
@@ -2,4 +2,11 @@
 
 def PrintOutput(string):
     print("OUTPUT " + string)
-
+    
+def LoadFile(fileloc):
+    f = open(fileloc)
+    alist = []
+    for line in f:
+        alist.append(line)
+    print(alist)
+    f.close()
