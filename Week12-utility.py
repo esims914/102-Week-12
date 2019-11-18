e62055cbcd754778518591c206bc205cd24d32a8
Trey Sims <esims@mymail.mines.edu>
2019-11-18 12:27:13 -0700

    added UpdateString

--- Week12-utility.py
+++ Week12-utility.py
@@ -10,3 +10,12 @@ def LoadFile(fileloc):
         alist.append(line)
     print(alist)
     f.close()
+
+def UpdateString(string, char, pos):
+    newstr = ''
+    for i in range(pos):
+        newstr += string[i]
+    newstr += char
+    for i in range(len(newstr)-1+len(char),len(string)):
+        newstr += string[i]
+    PrintOutput(newstr)
